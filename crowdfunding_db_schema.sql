628a14e4f0246e9d2f8d3f66a4286b5e754ff7ef
Nate Sheibley <n.sheibley@gmail.com>
2024-07-29 19:07:15 -0700

    added drop if exists

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -2,18 +2,21 @@
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
 
+DROP TABLE IF EXISTS category CASCADE;
 CREATE TABLE "category" (
     "category_id" VARCHAR(16)   NOT NULL,
     "category" VARCHAR(32)   NOT NULL,
     PRIMARY KEY ("category_id")
 );
 
+DROP TABLE IF EXISTS subcategory CASCADE;
 CREATE TABLE "subcategory" (
     "subcategory_id" VARCHAR(16)   NOT NULL,
     "subcategory" VARCHAR(32)   NOT NULL,
     PRIMARY KEY ("subcategory_id")
 );
 
+DROP TABLE IF EXISTS contacts CASCADE;
 CREATE TABLE "contacts" (
     "contact_id" INTEGER   NOT NULL,
     "first_name" VARCHAR(32)   NOT NULL,
@@ -22,6 +25,7 @@ CREATE TABLE "contacts" (
     PRIMARY KEY ("contact_id")
 );
 
+DROP TABLE IF EXISTS campaign CASCADE;
 CREATE TABLE "campaign" (
     "cf_id" INTEGER   NOT NULL,
     "contact_id" INTEGER NOT NULL REFERENCES contacts(contact_id),
@@ -50,3 +54,6 @@ SELECT * FROM contacts;
 SELECT * FROM subcategory;
 
 SELECT * FROM category;
+
+-- check dtype of a field in postgres https://stackoverflow.com/questions/3827828/how-to-check-type-of-value-in-postgres  
+SELECT pg_typeof(goal) FROM campaign;
